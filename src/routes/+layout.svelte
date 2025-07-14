0b13ae44fdd696dde8ea3e8372aa379478f03388
snowbalzz <alek0007@hz.nl>
2025-07-14 22:23:38 +0200

    init setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,16 @@
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	let { children } = $props();
 
-injectSpeedInsights();
+	injectSpeedInsights();
 </script>
 
-{@render children()}
+<!-- Navigator-->
+<div class="w-full h-10 bg-gray-100 flex flex-row items-center justify-start pl-4 gap-4">
+	<a href="/" class="text-gray-800 hover:text-gray-900 p-2">Home</a>
+	<a href="/know-destination" class="text-gray-800 hover:text-gray-900 p-2">Know Destination</a>
+	<a href="/new-experience" class="text-gray-800 hover:text-gray-900 p-2">New Experience</a>
+</div>
+
+<div class="w-full h-full m-auto flex flex-col items-center justify-center">
+	{@render children()}
+</div>
