ad9a6c597d89cd9d677babde0213954676b4c392
Laura <bira0001@hz.nl>
2025-07-20 20:11:19 +0300

    proof of concept form lol

--- src/routes/answer-know-destination/+page.svelte
+++ src/routes/answer-know-destination/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Good choice for the destination</h1>
+<script lang='ts'>
+  import { matchedLocations } from '$lib/stores/store.js';
+  import { onMount } from 'svelte';
+  import { get } from 'svelte/store';
+  
+  let results:any[] = [];
+
+  onMount(() => {
+    results = get(matchedLocations);
+  });
+</script>
+
+<h1>Results</h1>
+
+{#if results.length}
+  <ul>
+    {#each results as loc}
+      <li>{loc.name}</li>
+    {/each}
+  </ul>
+{:else}
+  <p>No locations matched your answers.</p>
+{/if}
--- src/routes/know-destination/+page.svelte
+++ src/routes/know-destination/+page.svelte
@@ -1,1 +1,56 @@
-<h1>I know where I am going</h1>
+<script>
+ import { matchedLocations } from '$lib/stores/store.js';
+  import { goto } from '$app/navigation';
+
+  let answer1 = '';
+  let answer2 = '';
+  let answer3 = '';
+
+  const locations = [
+    { name: 'Amsterdam', group: ['yes', 'red', 'morning'] },
+    { name: 'Rotterdam', group: ['no', 'blue', 'evening'] },
+    { name: 'Utrecht', group: ['yes', 'blue', 'morning'] }
+  ];
+
+  function submitForm() {
+    const matched = locations.filter(
+      (loc) =>
+        loc.group[0] === answer1 &&
+        loc.group[1] === answer2 &&
+        loc.group[2] === answer3
+    );
+
+    matchedLocations.set(matched); 
+    goto('/answer-know-destination');            
+  }
+</script>
+
+<form on:submit|preventDefault={submitForm}>
+  <label>
+    Do you like cities?  
+    <select bind:value={answer1}>
+      <option value="">--Choose--</option>
+      <option value="yes">Yes</option>
+      <option value="no">No</option>
+    </select>
+  </label>
+
+  <label>
+    Favorite color?
+    <select bind:value={answer2}>
+      <option value="">--Choose--</option>
+      <option value="red">Red</option>
+      <option value="blue">Blue</option>
+    </select>
+  </label>
+
+  <label>
+    Best time of day?
+    <select bind:value={answer3}>
+      <option value="">--Choose--</option>
+      <option value="morning">Morning</option>
+      <option value="evening">Evening</option>
+    </select>
+  </label>
+  <button type="submit">See Matches</button>
+</form>
